78bef86d3bea2ddd354e50584ff5bacdf0ea8eaa
kgovindu <kgovindu@apptio.com>
2019-11-30 23:46:48 +0530

    add comment

--- src/main/java/com/udf/udf/controller/TestController.java
+++ src/main/java/com/udf/udf/controller/TestController.java
@@ -23,7 +23,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.util.ArrayList;
 import java.util.List;
-
+// controller class
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {

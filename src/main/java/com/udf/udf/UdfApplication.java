fb2cc0284c20a6f806e62d45d5cced7e4ee865e1
govinduk <k.govind1232@gmail.com>
2019-12-01 17:16:25 +0530

    Update UdfApplication.java
    add comment  main class

--- src/main/java/com/udf/udf/UdfApplication.java
+++ src/main/java/com/udf/udf/UdfApplication.java
@@ -3,6 +3,8 @@ package com.udf.udf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// main  class
 @SpringBootApplication
 public class UdfApplication {
 
